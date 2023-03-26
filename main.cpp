6afc91d23374c403b8a40273cc7d763aaf50d20a
Cluley <oo43@yandex.ru>
2023-03-26 19:32:47 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "fibonacci.h"
+
+int main()
+{
+	int number = 0;
+	std::cout << "Введите число: ";
+	std::cin >> number;
+	std::cout << "Числа Фибоначчи: ";
+
+	for (int i = 0; i <= number; i++) {
+		std::cout << fibonacci(i) << ' ';
+	}
+
+	return 0;
+}
